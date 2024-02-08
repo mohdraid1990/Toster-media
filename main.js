614afebcbf4423bfff19f9585ec28d77b2fee429
mohdraid1990 <105244091+mohdraid1990@users.noreply.github.com>
2024-02-08 22:14:00 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,58 @@
-// Menu open 
-const menuIcon = document.querySelector('.menu-icon');
-const closeIcon = document.querySelector('.close');
-const menuList = document.querySelector('.menu-list');
+// Menu open
+const menuIcon = document.querySelector(".menu-icon");
+const closeIcon = document.querySelector(".close");
+const menuList = document.querySelector(".menu-list");
 
-menuIcon.addEventListener('click', toggleMenu);
-closeIcon.addEventListener('click', toggleMenu);
+menuIcon.addEventListener("click", toggleMenu);
+closeIcon.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  menuList.style.display = menuList.style.display === 'block' ? 'none' : 'block';
+  menuList.style.display =
+    menuList.style.display === "block" ? "none" : "block";
 }
 
 //  =================================== //
 
 // Slider
 
-const sliderImages = document.querySelectorAll('.slider img');
-const sliderIndicators = document.querySelectorAll('.slider-indicator');
-const sliderLine = document.querySelector('.slider-line');
+const sliderImages = document.querySelectorAll(".slider img");
+const sliderIndicators = document.querySelectorAll(".slider-indicator");
+const sliderLine = document.querySelector(".slider-line");
 
 let currentIndex = 0;
 
 function changeImage(index) {
-  sliderImages[currentIndex].style.display = 'none';
-  sliderIndicators[currentIndex].classList.remove('active');
+  sliderImages[currentIndex].style.display = "none";
+  sliderIndicators[currentIndex].classList.remove("active");
   currentIndex = index;
-  sliderImages[currentIndex].style.display = 'block';
-  sliderIndicators[currentIndex].classList.add('active');
+  sliderImages[currentIndex].style.display = "block";
+  sliderIndicators[currentIndex].classList.add("active");
 
   const indicatorWidth = sliderIndicators[0].offsetWidth;
   sliderLine.style.left = `${currentIndex * indicatorWidth}px`;
 }
 
 sliderIndicators.forEach((indicator, index) => {
-  indicator.addEventListener('click', () => changeImage(index));
+  indicator.addEventListener("click", () => changeImage(index));
 });
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
   const indicatorWidth = sliderIndicators[0].offsetWidth;
-
 });
 
 //  =================================== //
-
 
 // Update time
 
 function updateTime() {
   const now = new Date();
-  const hours = String(now.getHours()).padStart(2, '0');
-  const minutes = String(now.getMinutes()).padStart(2, '0');
-  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
   const currentTime = `${hours}:${minutes}:${seconds}`;
-  const timeElement = document.querySelector('.time');
+  const timeElement = document.querySelector(".time");
   timeElement.textContent = currentTime;
 }
-
 
 setInterval(updateTime, 1000);
 
@@ -63,17 +61,16 @@ updateTime();
 //  =================================== //
 
 // Close Apple Mac
-const offerBtn = document.querySelector('.offer-btn');
-const closeButton = document.querySelector('.close-mac');
-const appleMac = document.querySelector('.apple-mac');
+const offerBtn = document.querySelector(".offer-btn");
+const closeButton = document.querySelector(".close-mac");
+const appleMac = document.querySelector(".apple-mac");
 
-offerBtn.addEventListener('click', () => togglePopup(true));
-closeButton.addEventListener('click', () => togglePopup(false));
+offerBtn.addEventListener("click", () => togglePopup(true));
+closeButton.addEventListener("click", () => togglePopup(false));
 
 function togglePopup(open) {
-  appleMac.style.display = open ? 'block' : 'none';
+  appleMac.style.display = open ? "block" : "none";
 }
-
 
 //  =================================== //
 // Dropdown
@@ -92,7 +89,9 @@ function toggleDropdown() {
 function toggleText(button) {
   const textFooter = button.parentNode.previousElementSibling;
   if (textFooter) {
-    textFooter.style.maxHeight = textFooter.style.maxHeight ? null : textFooter.scrollHeight + "px";
+    textFooter.style.maxHeight = textFooter.style.maxHeight
+      ? null
+      : textFooter.scrollHeight + "px";
     button.innerHTML = textFooter.style.maxHeight ? "Read more" : "Read less";
   }
 }
